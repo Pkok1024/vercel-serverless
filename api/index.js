99a619ae96b5ce83b5de98910f582c099410404d
Pkok1024 <132034817+Pkok1024@users.noreply.github.com>
2023-12-29 13:55:47 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,67 +1,14 @@
-import express from 'express';
-import multer from 'multer';
-import { uploadFileToDiscord } from './dc';
-import fs from 'fs';
+import express from "express";
 
 const app = express();
 
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
-});
-
-app.get('/document', (req, res) => {
-  const apiDocContent = fs.readFileSync('api-doc.html', 'utf8');
-  res.send(apiDocContent);
-});
-
-app.get('/', (req, res) => {
-  const htmlContent = fs.readFileSync('index.html', 'utf8');
-  res.send(htmlContent);
-});
-
 app.get('/api', (req, res) => {
-  res.end(`Hello! Serverless`);
+	res.end(`Hello! Serverless`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-app.post('/api/upload-to-discord', upload.single('file'), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded.' });
-    }
-    if (req.file.size > 10 * 1024 * 1024) {
-      return res.status(400).json({ error: 'File size exceeds the 10MB limit.' });
-    }
-    const fileBuffer = req.file.buffer;
-
-    const result = await uploadFileToDiscord(fileBuffer);
-
-    const cleanedFileUrl = result.attachments[0].url.replace(/\?.*$/, '');
-
-    const discordResponse = {
-      message: 'File uploaded successfully.',
-      author: `${result.author.username}`,
-      responseCode: 200,
-      fileUrl: cleanedFileUrl,
-    };
-
-    console.log(`Notification:
-      ${discordResponse.message}
-      ${discordResponse.author}
-      Response Code: ${discordResponse.responseCode}
-      File URL: ${discordResponse.fileUrl}`);
-
-    res.status(200).json(discordResponse);
-  } catch (error) {
-    console.error('Error uploading to Discord:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+	const { slug } = req.params;
+	res.end(`Item: ${slug}`);
 });
 
 export default app;
