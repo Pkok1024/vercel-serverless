42bd77d131d129556f8bacde2238ba69c71030c3
Pkok1024 <132034817+Pkok1024@users.noreply.github.com>
2023-12-29 07:18:54 +0000

    a

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import multer from 'multer';
-import { uploadFileToDiscord } from '../src/dc';
-import fs from 'fs';
-
+import axios from "axios"
+import FormData from 'form-data';
 const app = express();
 const storage = multer.memoryStorage();
 const upload = multer({
@@ -18,21 +17,38 @@ app.get('/api/item/:slug', (req, res) => {
 	res.end(`Item: ${slug}`);
 });
 app.post('/api/upload-to-discord', upload.single('file'), async (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded.' });
-    }
-    if (req.file.size > 10 * 1024 * 1024) {
-      return res.status(400).json({ error: 'File size exceeds the 10MB limit.' });
-    }
-    const fileBuffer = req.file.buffer;
-    const result = await uploadFileToDiscord(fileBuffer);
-    const cleanedFileUrl = result.attachments[0].url.replace(/\?.*$/, '');
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded.' });
+  }
+
+  if (req.file.size > 10 * 1024 * 1024) {
+    return res.status(400).json({ error: 'File size exceeds the 10MB limit.' });
+  }
+
+  const fileBuffer = req.file.buffer;
+  const formData = new FormData();
+  formData.append('file', fileBuffer, { filename: req.file.originalname });
+
+  try {
+    const result = await axios.post('https://dollar.bivat22407.repl.co/api/upload-to-discord', formData, {
+      headers: {
+        ...formData.getHeaders(),
+      },
+    });
+
+    const cleanedFileUrl = result.data.attachments[0].url.replace(/\?.*$/, '');
     const discordResponse = {
       message: 'File uploaded successfully.',
-      author: `${result.author.username}`,
+      author: `${result.data.author.username}`,
       responseCode: 200,
       fileUrl: cleanedFileUrl,
     };
+
     res.status(200).json(discordResponse);
+  } catch (error) {
+    console.error('Error uploading file to Discord:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 });
+
 export default app;
